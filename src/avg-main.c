c146b7434c1df0c60c000b88ad1bee28a1b27cb9
Amy Ziru Liu <azl3@ecelinux-04.ece.cornell.edu>
2025-02-12 12:53:05 -0500

    fix sec04 code

--- src/avg-main.c
+++ src/avg-main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 int avg(int x, int y) {
-  // OOPS: a bug
-  int sum = x - y;
+  int sum = x + y;
   return sum / 2;
 }
 
 int main() {
-  int a = 10;
-  int b = 20;
+  int a;
+  int b;
+
+  scanf("%d %d", &a, &b);
+
   int c = avg(a, b);
   printf("average of %d and %d is %d\n", a, b, c);
   return 0;
